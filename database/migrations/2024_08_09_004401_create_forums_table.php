5896888e9f266b5da2175cafd54fa54037ce9524
DialloMaria <diallomariam0715@gmail.com>
2024-08-10 13:08:56 +0000

    creation du model Forum  avec l'ensemble de ces elements

---
+++ database/migrations/2024_08_09_004401_create_forums_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('description');
+            $table->integer('nombre de message');
+            $table->integer('nombre de vue');
+            $table->date('dateCreation');
+            $table->foreignIdFor(Domaine::class)->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forums');
+    }
+};
