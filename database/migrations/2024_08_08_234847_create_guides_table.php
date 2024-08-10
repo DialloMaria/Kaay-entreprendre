35f5d0306334ddcc4b99231cedab3517774b6d61
DialloMaria <diallomariam0715@gmail.com>
2024-08-10 13:05:32 +0000

    creation du model Guide avec l'ensemble de ces elements

---
+++ database/migrations/2024_08_08_234847_create_guides_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guides', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('contenu');
+            $table->date('datepublication');
+            $table->string('media');
+            $table->string('auteur');
+            $table->foreignIdFor(Domaine::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guides');
+    }
+};
