463b657bc43deabb74a4d886b97ee20a7cdb52a4
DialloMaria <diallomariam0715@gmail.com>
2024-08-10 17:02:59 +0000

    Mise en place du seeder pour categorie

--- database/seeders/CategorieSeeder.php
+++ database/seeders/CategorieSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Categorie;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategorieSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class CategorieSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Categorie::factory(10)->create();
     }
 }
