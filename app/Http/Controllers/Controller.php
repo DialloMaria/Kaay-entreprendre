26f0a11ff4956c905639196f24bb6c5db4e2d86c
souleymane97 <souleymane9700@gmail.com>
2024-08-10 22:59:45 +0000

    feat: Authentification

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+  public function  customJsonResponse($message, $data, $code = 200)
+    {
+      return response()->json([
+        "message" => $message,
+        "data" => $data
+      ], $code);
+    }
 }
 
