dc366f07db63883ad7329102968bfae85c22b10d
souleymane97 <souleymane9700@gmail.com>
2024-08-11 04:20:00 +0000

    feet: Inscription d'u entrepreneur dans un evenement

--- app/Models/UserEvent.php
+++ app/Models/UserEvent.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class UserEvent extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'event_id', 'status'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function evenement()
+    {
+        return $this->belongsTo(Evenement::class);
+    }
 }
