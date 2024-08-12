70aef686e2cdee1bccd5bd7d111467358096babe
souleymane97 <souleymane9700@gmail.com>
2024-08-12 09:48:24 +0000

    ende

---
+++ database/migrations/2024_08_09_084301_create_commentaires_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Guide;
+use App\Models\Evenement;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commentaires', function (Blueprint $table) {
+            $table->id();
+            $table->string('contenu');
+            $table->foreignIdFor(Guide::class)->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('modified_by')->nullable();
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->foreign('modified_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commantaires');
+    }
+};
