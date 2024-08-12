7db48fab7c979225c20125f4cd15b8cf4910da83
Amadou Barro <barroama23@gmail.com>
2024-08-12 17:30:50 +0000

    fix

---
+++ database/migrations/2024_08_08_244621_create_domaines_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\User;
+use App\Models\Categorie;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('domaines', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+
+            $table->foreignIdFor(Categorie::class)->constrained()->onDelete('cascade');
+            // SOFTDELE
+            $table->softDeletes();
+
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('modified_by')->nullable();
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->foreign('modified_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('domaines');
+    }
+};
